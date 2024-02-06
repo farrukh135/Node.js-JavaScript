9ba54c87fe5d231bdf34473605b250f44c259617
farrukh135 <130222158+farrukh135@users.noreply.github.com>
2024-02-06 10:20:37 +0000

    Control Flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,52 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
+
+const months = 4;
+
+switch (months) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    default:
+        console.log("Default case match");
+        break;
+}
